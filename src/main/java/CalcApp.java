3b986bf8714de0f16bab7290921de108fd481f7d
Glory Foncha <gloriane7767@gmail.com>
2025-03-14 21:58:27 +0100

    Initial commit

---
+++ src/main/java/CalcApp.java
@@ -0,0 +1,8 @@
+package com.gloriane; // Defines the package.
+
+import java.util.Scanner; // Imports Scanner to get user input.
+
+public class CalcApp { // Class name must match the filename.
+    public static void main(String[] args) { // Entry point of the program.
+        Scanner scanner = new Scanner(System.in); // Creates a Scanner object for input.
+
